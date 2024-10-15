1c8585fcfd3accd87476c7651ba5ddb482456bd0
CaioVilarouca <caiovilarouca@gmail.com>
2024-10-15 18:13:06 -0300

    update

--- gymform/src/application/Form.java
+++ gymform/src/application/Form.java
@@ -12,11 +12,11 @@ public class Form {
 	
 	// Tabale do IMC
 	public void tableImc (People people) {
-		if (imc >= 17.0 && imc < 18.4) {
+		if (imc > 17.0 && imc < 18.4) {
 			System.out.println("Status: ABAIXO DO PESO");
-		}else if (imc >= 18.5 && imc < 24.9) {
+		}else if (imc > 18.5 && imc < 24.9) {
 			System.out.println("Status: PESO NORMAL");
-		}else if (imc >= 25 && imc < 29.9) {
+		}else if (imc > 25 && imc < 29.9) {
 			System.out.println("Status: ACIMA DO PESO");
 		}else {
 			System.out.println("Status: OBESIDADE");
