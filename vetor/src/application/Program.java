ee4edc9a200e97b21c63fdb6f4cd69caf2b0400b
CaioVilarouca <caiovilarouca@gmail.com>
2024-10-24 08:33:24 -0300

    foreach

--- vetor/src/application/Program.java
+++ vetor/src/application/Program.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int number[] = new int[3];
-		
+		int number[] = new int[3];	
 		for (int i = 0; i < 3; i++) {
 			System.out.print(" :");
 			number[i] = scanner.nextInt();
 		}
 		
-		for (int i = 0; i < 3; i++) {
-			System.out.println("Número digitados:  " + 	number[i]);
+		for (int i : number) {
+			System.out.println("Número digitado: " + i);
 		}
 		scanner.close();
 	}
