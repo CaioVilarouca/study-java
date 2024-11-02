d02954a0d27d5a881fbc3f6e5eeb356b3276d2f3
CaioVilarouca <caiovilarouca@gmail.com>
2024-11-02 20:10:28 -0300

    clear 2.0

---
+++ register/src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.SystemOfRegister;
+
+public class Program {
+	public static void main (String args) {
+		SystemOfRegister systemOfRegister = new SystemOfRegister();
+		systemOfRegister.system();	
+	}
+}
---
+++ register/src/entities/SystemOfRegister.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class SystemOfRegister {
+
+	// Método principal 
+	public void system() {
+		star();
+	}
+	
+	public void star() {
+		System.out.println("Novo registro: ");
+	}
+}
