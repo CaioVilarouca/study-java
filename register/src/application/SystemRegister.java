9852a2ec7f2252392ae08763087cbdf066ab6032
CaioVilarouca <caiovilarouca@gmail.com>
2024-11-02 20:43:10 -0300

    upgrade

--- register/src/application/SystemRegister.java
+++ register/src/application/SystemRegister.java
@@ -1,5 +1,27 @@
 package application;
 
-public class SystemRegister {
+import java.util.Scanner;
 
+import entities.Product;
+
+public class SystemRegister {
+	Scanner scanner = new Scanner(System.in);
+	Product product = new Product();
+	
+	public void star() {
+		System.out.println("Novo registro de produto.\n");
+		newRegister();
+	}
+	
+	public void newRegister() {
+		System.out.print("Nome: ");
+		product.setNameProduct(scanner.nextLine());
+		
+		System.out.print("Valor R$ ");
+		product.setValueProduct(scanner.nextDouble());
+		
+		System.out.print("Quantidade: x");	
+		
+		scanner.close();
+	}
 }
