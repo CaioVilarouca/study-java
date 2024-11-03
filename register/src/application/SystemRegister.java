e84e74c16c48b816d55d64b827a16d8eca6e30b2
CaioVilarouca <caiovilarouca@gmail.com>
2024-11-03 19:41:01 -0300

    new register

--- register/src/application/SystemRegister.java
+++ register/src/application/SystemRegister.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import entities.Product;
@@ -14,14 +15,27 @@ public class SystemRegister {
 	}
 	
 	public void newRegister() {
-		System.out.print("Nome: ");
-		product.setNameProduct(scanner.nextLine());
+        System.out.print("Nome: ");
+        product.setNameProduct(scanner.nextLine());
 		
-		System.out.print("Valor R$ ");
-		product.setValueProduct(scanner.nextDouble());
-		
-		System.out.print("Quantidade: x");	
-		
-		scanner.close();
+        while (true) {
+            try {
+                System.out.print("Valor R$ ");
+                product.setValueProduct(scanner.nextDouble());
+                if (product.getValueProduct() <= 0) {
+                    throw new IllegalArgumentException("O valor não pode ser negativo ou null.");
+                }
+                break; // Sair do loop se a entrada for válida
+                
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, insira um valor numérico.");
+                scanner.next();
+                
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            
+            
+        }
 	}
 }
