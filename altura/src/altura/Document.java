efc1de660492ef0cbb24b163c1cdc1bc347f3d57
CaioVilarouca <caiovilarouca@gmail.com>
2025-01-17 20:36:41 -0300

    Up

---
+++ altura/src/altura/Document.java
@@ -0,0 +1,14 @@
+package altura;
+
+public class Document {
+	private String name;
+	private Integer ager;
+	private Double height;
+	
+	public Document(String name, Integer ager, Double height) {
+		super();
+		this.name = name;
+		this.ager = ager;
+		this.height = height;
+	}
+}
