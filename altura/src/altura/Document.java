b0cb4034926ce7c06924f51531a7d0b55468e9d3
CaioVilarouca <caiovilarouca@gmail.com>
2025-01-19 17:50:01 -0300

    Upgrade altura

--- altura/src/altura/Document.java
+++ altura/src/altura/Document.java
@@ -6,9 +6,50 @@ public class Document {
 	private Double height;
 	
 	public Document(String name, Integer ager, Double height) {
-		super();
 		this.name = name;
 		this.ager = ager;
 		this.height = height;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Integer getAger() {
+		return ager;
+	}
+
+
+
+	public void setAger(Integer ager) {
+		this.ager = ager;
+	}
+
+
+
+	public Double getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Document [name=" + name + ", ager=" + ager + ", height=" + height + "]";
+	}
+	
 }
--- altura/src/altura/Program.java
+++ altura/src/altura/Program.java
@@ -1,10 +1,35 @@
 package altura;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Word");
+		Scanner scanner = new Scanner(System.in);
+		
+		int number, ager;
+		String name;
+		double height;
+		
+		System.out.print("Quantas pessoas serão digitadas? ");
+		number = scanner.nextInt();
+		
+		Document[] document = new Document[number];
+		
+		for(int i = 0; i < document.length; i++) {
+			System.out.println("Dados da " + (i + 1)  + " primeira pessoa.");
+			System.out.print("Nome: ");
+			name = scanner.next();
+			System.out.print("Idade: ");
+			ager = scanner.nextInt();
+			System.out.print("Altura: ");
+			height = scanner.nextDouble();
+			
+			document[i] = new Document(name, ager, height);
+		}
+		
+		for (Document print : document) {
+		}
 	}
 
 }
