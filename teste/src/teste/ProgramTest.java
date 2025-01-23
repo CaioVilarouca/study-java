884f2ec3db9ba85d2946f0d9478d0e4abecf7efa
CaioVilarouca <caiovilarouca@gmail.com>
2025-01-22 21:35:37 -0300

    project test

---
+++ teste/src/teste/ProgramTest.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class ProgramTest {
+	public static void main(String[] args) {
+			int number = 21;
+			Object obj = number;
+			int numberX = (int) obj;
+			System.out.println(numberX);
+
+	}
+}
