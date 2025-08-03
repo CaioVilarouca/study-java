d4ba921c7f9ccad56061d7b4af3870d4e54aa2fe
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-02 21:43:52 -0300

    UP projeto banco

--- banco/src/programa/Inicio.java
+++ banco/src/programa/Inicio.java
@@ -12,5 +12,26 @@ public class Inicio {
 		Conta conta = new Conta(2004, "Thiago", 2098.0);
 		ContaComecial contaComecial = new ContaComecial(010101, "Emily", 45.000, 400.0);
 		ContaPoupanca contaPounaca = new ContaPoupanca(8080, "Arthur", 0.0, 0.5);
+		
+		// UPCASTING 
+		Conta conta01 = contaComecial;
+		//    Super Class    Sub Class
+		Conta conta02 = new ContaComecial(2003, "Caio", 25.0, 50.0);
+		Conta conta03 = new ContaPoupanca(1098, "Jamison", 100.0, 0.0);
+		
+		// DOWCASTING
+		ContaComecial conta04 = (ContaComecial)conta02;
+		conta04.limite(100.0);
+
+		if (conta04 instanceof ContaComecial) {
+			System.out.println("POSITIVO!");
+		}
+		
+
+		if (conta02 instanceof ContaPoupanca) {
+			System.out.println("POSITIVO!");
+		}else {
+			System.out.println("NEGATIVO!");
+		}
 	}
 }
