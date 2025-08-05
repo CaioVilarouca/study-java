54f9c92eee082aff56667ba1824ae30f58285575
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-05 16:37:53 -0300

    up

---
+++ banco/src/contasUsuarios/Conta.java
@@ -0,0 +1,37 @@
+package contasUsuarios;
+
+public class Conta {
+	private Integer numero;
+	private String titular;
+	protected Double saldo;
+	
+	public Conta() {
+		
+	}
+
+	public Conta(Integer numero, String titular, Double saldo) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+	
+	public void retirada(Double amount) {
+		saldo -= amount;
+	}
+	
+	public void deposito(Double amount) {
+		saldo += amount;
+	}
+}
---
+++ banco/src/contasUsuarios/ContaPJ.java
@@ -0,0 +1,33 @@
+package contasUsuarios;
+
+public class ContaPJ extends Conta {
+	private Double LimiteEmp;
+	
+	
+	public ContaPJ() {
+		
+	}
+
+
+	public ContaPJ(Integer numero, String titular, Double saldo, Double limiteEmp) {
+		super(numero, titular, saldo);  // Construtor da SuperClass 
+		LimiteEmp = limiteEmp;
+	}
+
+
+	public Double getLimiteEmp() {
+		return LimiteEmp;
+	}
+
+
+	public void setLimiteEmp(Double limiteEmp) {
+		LimiteEmp = limiteEmp;
+	}
+	
+	public void limte(Double amount) {
+		if (amount <= LimiteEmp) {
+			saldo  += amount - 10.0;
+		}
+	}
+	
+}
