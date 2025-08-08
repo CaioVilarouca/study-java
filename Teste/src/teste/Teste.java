e7e0d257df05899af947afad5d327c96201ebcc0
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-08 14:13:54 -0300

    Up

---
+++ Teste/src/teste/Teste.java
@@ -0,0 +1,19 @@
+package teste;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        double mediaMinima = 9.0;
+
+		        double mediaAluno1 = 7.5;
+		        boolean aprovado1 = mediaAluno1 >= mediaMinima;
+
+		        System.out.println(aprovado1);
+		    }
+		
+
+	
+
+}
