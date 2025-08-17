f580a816394b93beffa9367e1a8b5cbdc73ac5cf
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-16 23:06:04 -0300

    Up teste Try Catch method02

--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -4,21 +4,27 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Teste {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		new Teste().method2(scanner);
+		scanner.close();
+
+	}
+	
+	public void method2(Scanner scanner) {
 		
-		String[] vect = scanner.nextLine().split(" ");
+		String[] vect = scanner.nextLine().split(" "); // Add dados no vector na mesma linha usando espaçamento
 		
 		int position = scanner.nextInt();
 		
 		try {
 			System.out.printf("Position %d value: %s", position, vect[position]);
 			
-		} catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) { // Quanto tenta acessar uma posição que não existe  
 			System.out.println("Valor INVALIDO!");
 			
-		} catch (InputMismatchException e) {
+		} catch (InputMismatchException e) { // Quanto valor digitado não é do tipo INT
 			System.out.println("Esse campo e aceito apenas numeros.");
 		} finally {
 			int cont = 0;
@@ -29,6 +35,5 @@ public class Teste {
 			
 			System.out.println("\nVect tem " + cont);
 		}
-		scanner.close();
 	}
 }
