7562fd239f42277f147a9cfe77ca455c94cbb8be
Caio <caiovilarouca@gmail.com>
2026-03-31 18:42:08 -0300

    feat

---
+++ Estruturas Repetitivas/exe05/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int x=scanner.nextInt();
+		for(int i=0;i<x;i++) {
+			if(!(i % 2==0))
+				System.out.println(i);
+		}
+		scanner.close();
+	}
+}
